c0c9de0e88ac8b2a30c92c1eee279c7be63c8881
Teacher-Jorge <79639664+Teacher-Jorge@users.noreply.github.com>
2023-03-14 17:45:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,34 @@ let btnAre = document.getElementById("btnAre")
 let btnArent = document.getElementById("btnArent")
 let answer = document.getElementById('answer')
 
-btnIs.addEventListener('click', function() {
-    answer.innerHTML = 'is'
-    btnIs.style.background = 'lime'
+btnAre.addEventListener('click', function() {
+    let audio = document.getElementById('song')
+    audio.play()
+    answer.innerHTML = 'are'
+    btnAre.style.background = 'lime'
+    answer.style.color = 'lime'
+    btnAre.disabled = 'true'
+    btnIs.disabled = 'true'
+    btnArent.disabled = 'true'
 })
+
+btnIs.addEventListener('click', function() {
+    let audio = document.getElementById('pop')
+    audio.play()
+    answer.innerHTML = 'is'
+    btnIs.style.background = 'red'
+    answer.style.color = 'red'
+    btnIs.disabled = 'true'
+})
+
+btnArent.addEventListener('click', function() {
+    let audio = document.getElementById('pop')
+    audio.play()
+    answer.innerHTML = "aren't"
+    btnIs.style.background = 'red'
+    answer.style.color = 'red'
+    btnArent.disabled = 'true'
+})
+
+
 
